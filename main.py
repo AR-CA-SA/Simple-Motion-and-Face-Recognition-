0179cc956e8729e47b8e26172a5069bbefc2f0b2
Juan C SEFA <118149460+AR-CA-SA@users.noreply.github.com>
2024-12-03 06:51:49 -0500

    Update main.py
    desc1

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ from motion import motionMain
 #main interface to star up the program
 
 print("-Press O to start up the program-")
-print("-Press X to kill the program-")
+print("-Press X to terminate and exit the program")
+print("-Press Z to exit the program")
 
-inp = input("-: ")
-togg = True
-
-
-
-while(togg == True):
+while True:
+    inp = input("-: ")
     if inp == 'O' or inp == 'o':
         motionMain()
+        break
+    elif inp =='z' or inp =='Z':
+        print("Goodbye!")
+        break
     else:
         print("incorrect input")
-        inp = input('-:')
-print("Goodbye!")
+    
